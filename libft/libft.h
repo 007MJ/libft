12a09337b0d2a56108886d07eb80a0e065be7719
007MJ <112676830+007MJ@users.noreply.github.com>
2022-10-29 19:39:28 -0400

    strjion

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 12:50:16 by mnshimiy          #+#    #+#             */
-/*   Updated: 2022/10/28 16:59:50 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2022/10/29 17:40:44 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
--- libft/src/ft_strjoin.c
+++ libft/src/ft_strjoin.c
@@ -1,37 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/28 13:20:40 by mnshimiy          #+#    #+#             */
-/*   Updated: 2022/10/29 17:32:07 by mnshimiy         ###   ########.fr       */
+/*   Created: 2022/10/29 17:39:48 by mnshimiy          #+#    #+#             */
+/*   Updated: 2022/10/29 19:18:23 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	c_start;
-	char	*ptr;
+	int	s1len;
+	int	s2len;
 
-	c_start = start;
-	ptr = malloc((len + 1) * sizeof(*s));
-	if (!ptr)
-		return (ptr);
-	i = 0;
-	if (c_start <= ft_strlen(s))
-	{
-		while (s[c_start] != '\0' && i < len)
-		{
-			ptr[i] = s[start];
-			start++;
-			i++;
-		}
-	}
-	ptr[i] = '\0';
-	return (ptr);
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
 }
