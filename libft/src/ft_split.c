5a05d6d393fb34b5847311030fab7c7f63088ec1
007MJ <112676830+007MJ@users.noreply.github.com>
2022-11-03 10:07:15 -0400

    after 3 dark now i can see

--- libft/src/ft_split.c
+++ libft/src/ft_split.c
@@ -1,37 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/18 20:01:41 by mnshimiy          #+#    #+#             */
-/*   Updated: 2022/11/02 16:05:28 by mnshimiy         ###   ########.fr       */
+/*   Created: 2022/11/01 15:33:24 by mnshimiy          #+#    #+#             */
+/*   Updated: 2022/11/02 16:05:38 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	*ft_memmove(void *dst, const void *src, size_t len)
-{
-	size_t	i;
-	char	*csrc;
-	char	*cdst;
-
-	i = 0;
-	csrc = (char *)src;
-	cdst = (char *)dst;
-	if (cdst > csrc)
-	{
-		while (len > 0)
-		{
-			cdst[len - 1] = csrc[len - 1];
-			len--;
-		}
-	}
-	else
-	{
-		ft_memcpy(cdst, csrc, len);
-	}
-	return (dst);
-}
--- libft/src/ft_strtrim.c
+++ libft/src/ft_strtrim.c
@@ -6,61 +6,51 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 08:57:20 by mnshimiy          #+#    #+#             */
-/*   Updated: 2022/10/31 18:26:54 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2022/11/01 14:33:44 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	lefti(char *s1, char const *set)
-{
-	int	i;
-
-	i = 0;
-	//rendu check a l'interieur !
-	while (s1[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-static	int	righti(char *s1, char const *set)
+static int	checkinset(char c, char const *set)
 {
 	int	j;
-	int	i;
 
-	i = ft_strlen(s1);
-	i = i - 1;
 	j = 0;
-	while (s1[i] == set[j])
+	while (set[j])
 	{
-		i--;
+		if (set[j] == c)
+		{
+			return (1);
+		}
 		j++;
 	}
-	return (i);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		j;
-	int		i;
-	char	*newstr;
-	int		lenghs1;
+	size_t		left;
+	size_t		right;
+	char		*newstr;
+	size_t		i;
 
-	lenghs1 = ft_strlen(s1);
-	j = 0;
-	newstr = malloc ((lenghs1 + 1) * sizeof(s1));
+	left = 0;
+	i = 0;
+	while (s1[left] && checkinset(s1[left], set) == 1)
+		left++;
+	right = ft_strlen(s1);
+	while (left < right && checkinset(s1[right - 1], set) == 1)
+		right--;
+	newstr = (char *)malloc ((right - left + 1) * sizeof(*s1));
 	if (!newstr)
 		return (NULL);
-	i = lefti((char *)s1, set);
-	lenghs1 = righti((char *)s1, set);
-	while (j <= s1[lenghs1])
+	while (left < right)
 	{
-		newstr[j] = s1[i];
-		j++;
+		newstr[i] = s1[left];
 		i++;
+		left++;
 	}
-	newstr[j] = '\0';
+	newstr[i] = '\0';
 	return (newstr);
 }
