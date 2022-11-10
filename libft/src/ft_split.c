368b786d590d9240bd2a52adec7463a311d37115
007MJ <112676830+007MJ@users.noreply.github.com>
2022-11-10 10:56:57 -0500

    split done

--- libft/src/ft_split.c
+++ libft/src/ft_split.c
@@ -6,13 +6,27 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 13:34:07 by mnshimiy          #+#    #+#             */
-/*   Updated: 2022/11/09 18:15:27 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2022/11/10 10:52:10 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	nbrworld(char *str, char c)
+static char	**ft_free(char **tab)
+{
+	size_t	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (0);
+}
+
+static int	nbrworld(char const *str, char c)
 {
 	int	i;
 	int	word;
@@ -21,40 +35,47 @@ static int	nbrworld(char *str, char c)
 	word = 0;
 	while (str[i])
 	{
-		if (str[i] == c)
-		{
-			if (str[i -1] != c)
-			{
-				word++;
-			}
-		}
-		i++;
+		while (str[i] == c && str[i])
+			i++;
+		while (str[i] != c && str[i])
+			i++;
+		if (str[i -1] != c)
+			word++;
 	}
 	return (word);
 }
 
-static **yig(char const *s, char c, int word)
+static char	**yig(char **yp, char const *s, char c)
 {
-	  int i;
-  int star;
-  int len;
-  i = 0;
-  star = 0;
-  len = 0;
-  while (s[i])
-  {
-    if (s[i] != c)
-    {
-      star = i;
-    }
-    if (s[i] != c && s[i -1] != c)
-        len++;
+	int	i;
+	int	star;
+	int	len;
 
-    i++;
-  }
+	i = 0;
+	star = 0;
+	len = 0;
+	while (s[i] && len < nbrworld(s, c))
+	{
+		while (s[i] == c && s[i])
+			i++;
+		star = i;
+		while (s[i] != c && s[i])
+			i++;
+		yp[len] = ft_substr(s, star, i - star);
+		if (!yp)
+			return (ft_free(yp));
+		len++;
+	}
+	yp[len] = 0;
+	return (yp);
 }
 
-char **ft_split(const char *s, char c)
+char	**ft_split(const char *s, char c)
 {
+	char	**split;
 
+	split = malloc (sizeof(char *) * (nbrworld(s, c) + 1));
+	if (!split)
+		return (0);
+	return (yig(split, s, c));
 }
--- libft/src/ft_substr.c
+++ libft/src/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 13:20:40 by mnshimiy          #+#    #+#             */
-/*   Updated: 2022/10/29 17:32:07 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2022/11/10 10:36:34 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
-	size_t	c_start;
 	char	*ptr;
 
-	c_start = start;
-	ptr = malloc((len + 1) * sizeof(*s));
+	i = 0;
+	if (start < ft_strlen(s))
+		while (i < len && s[start + i])
+				i++;
+	ptr = malloc((i + 1) * sizeof(*s));
 	if (!ptr)
 		return (ptr);
 	i = 0;
-	if (c_start <= ft_strlen(s))
+	if (start <= ft_strlen(s))
 	{
-		while (s[c_start] != '\0' && i < len)
+		while (s[start] != '\0' && i < len)
 		{
 			ptr[i] = s[start];
 			start++;
