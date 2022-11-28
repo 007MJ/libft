94933234918b07a8f1e17b38040657e89b0f7e7b
Mijiza Nshimiye <mnshimiy@c3r10p6.42quebec.com>
2022-11-28 11:00:21 -0500

    calloc better

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 13:38:28 by mnshimiy          #+#    #+#             */
-/*   Updated: 2022/11/26 15:38:02 by mnshimiy         ###   ########.fr       */
+/*   Updated: 2022/11/28 10:52:03 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	*ptr;
+	char	*ptr;
 
 	if (count == 0 && size == 0)
 	{
@@ -23,7 +23,7 @@ void	*ft_calloc(size_t count, size_t size)
 	}
 	ptr = malloc(count * size);
 	if (!ptr)
-		return (ptr);
+		return (0);
 	ft_bzero(ptr, (count * size));
 	return (ptr);
 }
